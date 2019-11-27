2f1412ff377b42ae2ace4fdfc190eca5aa8d6011
Shay Nehmad <shay.nehmad@guardicore.com>
2019-11-27 13:14:59 +0200

    Added the first stage of the challenge.

---
+++ .travis/flag.sh
@@ -0,0 +1,1 @@
+echo "The flag is rollinia-flints-lut"
--- .travis/runme.sh
+++ .travis/runme.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 
-echo "*********************************************************************"
-echo "Looks like nothing happened yet!"
-echo "*********************************************************************"
+function printaline {
+    echo "*********************************************************************"
+}
+
+function bad {
+    echo $1
+    printaline
+    exit 1
+}
+
+printaline
+echo "Looks like you've started the challenge. Good luck!"
+
+# Check file existence. 
+if [ ! -f alice.txt ]; 
+then bad "Alice is missing! Try again.";
+fi
+
+if [ ! -f bob.txt ]; 
+then bad "Bob is missing! Try again.";
+fi
+
+# Check git log length
+commit_amount=$(git log start-here.. --oneline | wc -l)
+if [ $commit_amount -ne 1 ];
+then bad "The files should have been added in a single commit, but I've found ${commit_amount} commits in the log. To reset and try again, delete the local start-here branch and checkout the original start-here branch again.";
+fi
+
+# Everything's OK
+printaline
 exit 0
