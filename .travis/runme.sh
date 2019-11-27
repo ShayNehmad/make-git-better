b9a111b94164176afa47b45e0d081f11546ef3a0
Shay Nehmad <shay.nehmad@guardicore.com>
2019-11-27 22:35:04 +0200

    Added basic functions to travis script

--- .travis/runme.sh
+++ .travis/runme.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 
-echo "*********************************************************************"
+
+function printaline {
+    echo "*********************************************************************"
+}
+
+function flag {
+    echo "              ______________ "
+    echo " _____________\            / "
+    echo "|               __        /  "
+    echo "|  __      __  | _       /   "
+    echo "| |__ |   |__| |__|      \   "
+    echo "| |   |__ |  |            \  "
+    echo "|                __________\ "
+    echo "|________________\           "
+    echo "|                            "
+    echo "|                            "
+    echo "|                            "
+    echo "|                            "
+    echo "|                            "
+    echo "|                            "
+    echo "|                            "
+    echo "|                            "
+    echo "|     The flag is...         "
+    echo "|                            "
+    echo $1
+}
+
+function bad {
+    echo "Something went wrong!"
+    echo $1
+    printaline
+    exit 1
+}
+
+printaline
 echo "Looks like nothing happened yet!"
-echo "*********************************************************************"
+printaline
 exit 0
