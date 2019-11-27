3f55c4170c3ab03e1211779d4a37577bdda75a82
Barak Nehmad <bnehmad@gmail.com>
2019-11-28 00:00:14 +0200

    Added function which generates branch names and exported magic numbers to consts.

--- .management/branch_names.py
+++ .management/branch_names.py
@@ -1,30 +1,50 @@
 import random
 import os
 
+SET_LENGTH = 10
+RANDOM_WORDS_PER_NAME = 3
+MINIMUM_WORDS_LENGTH = 2
+MAXIMUM_WORDS_LENGTH = 9
+
 
 def main():
     print("I'm the branch name generator.\nHere are some names for you:\n")
-    # Think about changing this to movie names from imdb database
     words = load_words()
     print(len(words))
 
     # Filter to mostly normal pronounceable words
-    words = set(filter(lambda x: 2 < len(x) < 9, words))
+    words = set(filter(lambda x: MINIMUM_WORDS_LENGTH < len(x) < MAXIMUM_WORDS_LENGTH, words))
 
     print(len(words))
 
-    while True:
-        random_name = '-'.join(random.sample(words, 3))
-        print(random_name)
-        _ = input()
+    print(generate_words_set(words))
 
     
 def load_words():
+    """
+    Loads words from english-words repo.
+    """
     with open(os.path.join('english-words', 'words_alpha.txt')) as word_file:
         valid_words = set(word_file.read().split())
 
     return valid_words
 
 
+def generate_names_set(words, set_length=SET_LENGTH, random_words_per_name=RANDOM_WORDS_PER_NAME):
+    """
+    Generates set of branch names.
+    """
+    names_set = []
+    i = 0
+    while set_length > i:
+        random_name = '-'.join(random.sample(words, random_words_per_name))
+        if random_name in names_set:
+            continue
+        names_set.append(random_name)
+        i += 1
+    return set(names_set)
+
+
 if __name__ == "__main__":
     main()
+
