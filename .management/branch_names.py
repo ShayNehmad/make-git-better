39a472101ffd94d97acab63bf5c5156495e6c094
Barak Nehmad <bnehmad@gmail.com>
2019-11-29 00:15:48 +0200

    Added git capabilities to script. Now generated branch names are pushed to remote.

--- .management/branch_names.py
+++ .management/branch_names.py
@@ -1,5 +1,7 @@
-import random
 import os
+import random
+
+import git
 
 SET_LENGTH = 10
 RANDOM_WORDS_PER_NAME = 3
@@ -16,13 +18,22 @@ def main():
     words = set(filter(lambda x: MINIMUM_WORDS_LENGTH < len(x) < MAXIMUM_WORDS_LENGTH, words))
 
     print(len(words))
+    names_set = generate_names_set(words)
+    print(names_set)
 
-    print(generate_words_set(words))
+    repo_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    repo = git.Repo(repo_dir)
+
+    for branch_name in names_set:
+        create_and_push_branch(repo, branch_name)
+        print("Pushed {0}, checking out dev".format(branch_name))
+        repo.git.checkout('dev')
 
     
 def load_words():
     """
     Loads words from english-words repo.
+    :return: Set of valid words.
     """
     with open(os.path.join('english-words', 'words_alpha.txt')) as word_file:
         valid_words = set(word_file.read().split())
@@ -33,6 +44,10 @@ def load_words():
 def generate_names_set(words, set_length=SET_LENGTH, random_words_per_name=RANDOM_WORDS_PER_NAME):
     """
     Generates set of branch names.
+    :param words: Set of words to sample from.
+    :param set_length: Length of returned set.
+    :param random_words_per_name: Amount of random words per branch name.
+    :return:
     """
     names_set = []
     i = 0
@@ -43,6 +58,26 @@ def generate_names_set(words, set_length=SET_LENGTH, random_words_per_name=RANDO
         names_set.append(random_name)
         i += 1
     return set(names_set)
+
+
+def create_and_push_branch(repo, branch_name):
+    """
+    Creates a branch with the given name,
+    :param repo: Git repo object.
+    :param branch_name: Name of branch to create.
+    :return:
+    """
+
+    git_cmd = repo.git
+    git_cmd.checkout('HEAD', b=branch_name)
+    file_path = os.path.join(repo.working_dir, "{0}.txt".format(branch_name))
+
+    with open(file_path, 'w') as fd:
+        fd.write(branch_name)
+
+    repo.index.add(file_path)
+    repo.index.commit(branch_name)
+    git_cmd.push(['--set-upstream', 'origin', branch_name])
 
 
 if __name__ == "__main__":
