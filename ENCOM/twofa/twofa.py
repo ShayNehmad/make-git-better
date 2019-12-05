089fa3c9e2998e5fdd2f3c658c7b88e237c97cb3
Shay Nehmad <shay.nehmad@guardicore.com>
2019-12-05 12:26:06 +0200

    Checking the user password.
    Merge me into ethers-kalongs-asylum

--- ENCOM/twofa/twofa.py
+++ ENCOM/twofa/twofa.py
@@ -5,5 +5,9 @@ def check_2fa_login(password):
     # Need to get env variable
     twofa_password = os.environ["2FA"]
 
-    # TODO here we'll check the 2FA password
-    return 1 == 2
+    # TODO here we'll check the 2FA password.
+    # TODO Merge me into ethers-kalongs-asylum
+    import hashlib
+    n = hashlib.sha512()
+    n.update(password.encode())
+    return 1 == n.hexdigest()
