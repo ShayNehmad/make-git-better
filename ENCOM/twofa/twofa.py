4e9843b92b042228b17873c6a3e423fdc4a305ff
Shay Nehmad <shay.nehmad@guardicore.com>
2019-12-05 12:56:22 +0200

    merge me into ethers branch plz

--- ENCOM/twofa/twofa.py
+++ ENCOM/twofa/twofa.py
@@ -3,7 +3,7 @@ import os
 
 def check_2fa_login(password):
     # Need to get env variable
-    twofa_password = os.environ["2FA"]
+    twofa_password = os.environ["TWO_FA"]
 
     # TODO here we'll check the 2FA password.
     # We need to check the 2FA password from the environment variable against the user-provided password. Also, I heard that using hashes is secure - let's do that!
