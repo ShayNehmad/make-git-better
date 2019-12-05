78f94395f3bbc933037f8d2e1215aadc3a751935
Shay Nehmad <shay.nehmad@guardicore.com>
2019-12-05 12:17:17 +0200

    Started working on the 2fa. The rest of the work is over at grubbery-unopenly-unhushed, need to merge it...

--- ENCOM/twofa/twofa.py
+++ ENCOM/twofa/twofa.py
@@ -5,5 +5,9 @@ def check_2fa_login(password):
     # Need to get env variable
     twofa_password = os.environ["2FA"]
 
-    # TODO here we'll check the 2FA password
-    return 1 == 2
+    # TODO here we'll check the 2FA password.
+    # TODO Need to merge the rest of the work over from `grubbery-unopenly-unhushed`. But I'm just so lazy... 💤
+    import hashlib
+    m = hashlib.sha512()
+    m.update(twofa_password)
+    return m.hexdigest() == 2
