eef1a8b1ff8a5c37f2b43882dbcce3b0aeafb753
Shay Nehmad <shay.nehmad@guardicore.com>
2019-12-05 22:46:12 +0200

    Update activation.py

--- ENCOM/rinzler/activation.py
+++ ENCOM/rinzler/activation.py
@@ -3,5 +3,5 @@ import sys
 
 def activate_rinzler():
     print("Trying to activate Rinzler...")
-    #return True  # TODO uncomment this line, and delete the next line
+    return True  # TODO uncomment this line, and delete the next line
     sys.exit(1)
