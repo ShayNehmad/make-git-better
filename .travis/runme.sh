5a337e389bf80f1bd5b36bd30e14c090a584992b
Shay Nehmad <shay.nehmad@guardicore.com>
2019-12-05 09:54:07 +0200

    Refactored runme.sh to use functions better

--- .travis/runme.sh
+++ .travis/runme.sh
@@ -33,6 +33,23 @@ function bad {
     exit 1
 }
 
+function echo_tag_commit_hash {
+    echo $( git log -1 --format=format:"%h" $1 )
+}
+
+function echo_parents_amount {
+    echo $( git cat-file -p $1 | grep parent | wc -l )
+}
+
+function is_merge_commit {
+    if [ echo_parents_amount $1 -eq 2 ];
+    then
+        true;
+    else
+        false;
+    fi
+}
+
 printaline
 echo "Testing ENCOM basic access script."
 
@@ -45,23 +62,21 @@ fi
 
 # Check if the correct parent is a merge commit
 git fetch --tags -q
-rollinia_commit_hash=$( git log -1 --format=format:"%h" rollinia-flints-lut-tag )
+rollinia_commit_hash=$( echo_tag_commit_hash rollinia-flints-lut-tag )
 parent_1=$( git log -1 | head -2 | tail -1 | awk '{ print $2 }' )
-parent_1_parents_amount=$( git cat-file -p ${parent_1} | grep parent | wc -l )
+parent_1_parents_amount=$( echo_parents_amount ${parent_1} )
 parent_2=$( git log -1 | head -2 | tail -1 | awk '{ print $3 }' )
-parent_2_parents_amount=$( git cat-file -p ${parent_2} | grep parent | wc -l )
+parent_2_parents_amount=$( echo_parents_amount ${parent_2} )
 
 echo "parent 1 hash: " $parent_1 
-echo "parent 1 # of parents: " $parent_1_parents_amount
 echo "parent 2 hash: " $parent_2
-echo "parent 2 # of parents: " $parent_2_parents_amount
 
 echo "Let's look at the log..."
 git log --oneline --graph --decorate -n 4
 
-if [ $parent_1 != $rollinia_commit_hash -a $parent_1_parents_amount -eq 1 ];
+if [ $parent_1 != $rollinia_commit_hash -a is_merge_commit $parent_1 ];
 then bad "Your commit isn't a merge commit! You must solve this stage using a merge. Try again.";
-elif [ $parent_2 != $rollinia_commit_hash -a $parent_2_parents_amount -eq 1 ];
+elif [ $parent_2 != $rollinia_commit_hash -a is_merge_commit $parent_2 ];
 then bad "Your commit isn't a merge commit! You must solve this stage using a merge. Try again.";
 fi
 
