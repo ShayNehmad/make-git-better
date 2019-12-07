813e044a45fdd4541744af4e38f274e5eb2296f3
Shay Nehmad <shay.nehmad@guardicore.com>
2019-12-07 13:22:35 +0200

    Stage 1: updated test script.

--- .travis/runme.sh
+++ .travis/runme.sh
@@ -33,6 +33,10 @@ function bad {
     exit 1
 }
 
+function echo_tag_commit_hash {
+    echo $( git log -1 --format=format:"%h" $1 )
+}
+
 printaline
 echo "Looks like you've started the challenge. Good luck!"
 
@@ -53,6 +57,34 @@ commit_amount=$( git log start-here-tag.. --oneline | wc -l )
 if [ $commit_amount -ne 2 ];
 then bad "The files should have been added in a single commit, but I've found ${commit_amount} commits (including the merge commit that the pull request is trying to do) in the log. To reset and try again, delete the local start-here branch, checkout the original start-here branch again and try again.";
 fi
+
+# Check if only 2 files were changed
+start_here_commit_hash=$( echo_tag_commit_hash start-here-tag )
+
+parent_1=$( git log -1 | head -2 | tail -1 | awk '{ print $2 }' )
+parent_2=$( git log -1 | head -2 | tail -1 | awk '{ print $3 }' )
+
+echo "Let's look at the log..."
+git log --oneline --graph -n 4 --decorate
+
+echo "parent 1 hash: " $parent_1
+echo "parent 2 hash: " $parent_2
+
+# which parent is the user's commit? 
+users_commit_hash=""
+if [[ $parent_1 = $start_here_commit_hash ]] # parent1 is NOT the user's commit
+then users_commit_hash=$parent_2
+else users_commit_hash=$parent_1
+fi
+
+echo "User's commit was ${users_commit_hash}"
+
+# We know that there's only one commit in the changes - otherwise it would have failed before.
+number_of_files_changed=$( git diff --stat ${users_commit_hash} ${users_commit_hash}~1 | grep "files changed" | awk ' {print $1} ' ) 
+if [[ $number_of_files_changed -ne 2 ]]
+then bad "More than 2 files were changed! Only add alice.txt and bob.txt. Check out the original branch and try again.";
+fi
+
 
 # Everything's OK
 flag rollinia-flints-lut
